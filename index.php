b82ec60bd14d3069cd3dc3f0f773aa5548f4bb09
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-05-03 11:40:14 +0200

    [Adding files]
    Adding of php, js, css and assets files.

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="FR-fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Font : Outfit -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- Font awesome -->
+    <script src="https://kit.fontawesome.com/ae29616080.js" crossorigin="anonymous"></script>
+    <title>Todo list</title>
+</head>
+<body>
+
+<header>
+    <h1>À Faire</h1>
+</header>
+<main>
+
+</main>
+<footer>
+    <p>2022 - Made with <i class="fa-solid fa-heart"></i></p>
+</footer>
+<script src="app.js"></script>
+</body>
+</html>
