ca906640afd4197e5f35b9ac6de21f92f4ed83f6
AdrienSoriano <octavarium@PORT202109-043.localdomain>
2022-05-12 15:30:25 +0200

    [UP]
    Mise en place des listes avec la base de données.

--- app.js
+++ app.js
@@ -4,15 +4,9 @@ btn.forEach(element => {
     element.addEventListener("click", ()=>{
         let check = element;
         
-        if(check.style.backgroundImage = "url('Assets/Media/validation.png')"){
+        if((check.style.backgroundImage == "url('Assets/Media/validation.png')") && (check.style.border = 'none')){
             check.style.backgroundImage = "none";
-            check.style.border = 'solid 0.5px grey';check.style.backgroundImage = "url('Assets/Media/validation.png')";
-            check.style.border = 'none';
-        }
-        else
-        if(check.style.backgroundImage = "none"){
-            check.style.backgroundImage = "url('Assets/Media/validation.png')";
-            check.style.border = 'none';
+            check.style.border = 'solid 0.5px grey';
         }
         console.log(element);
     })
--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
-include 'logins/connect.php';
+include 'logins/login.php';
+
+$showAll = "SELECT taches.nom, statut.nom_du_statut, statut.url_image FROM taches INNER JOIN statut ON taches.id_statut = statut.id;";
+$showFinished = "SELECT taches.nom, statut.nom_du_statut, statut.url_image FROM taches INNER JOIN statut ON taches.id_statut = statut.id WHERE taches.id_statut = 2;";
+$showUnfinished = "SELECT taches.nom, statut.nom_du_statut, statut.url_image FROM taches INNER JOIN statut ON taches.id_statut = statut.id WHERE taches.id_statut = 1";
+
+/* $all = $_POST['all'];
+$finished = $_POST['finished'];
+$unfinished = $_POST['unfinished'];
+$status = $_POST['statut']; */
+try{
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $stmt1 = $conn->query($showAll);
+    $stmt2 = $conn->query($showFinished);
+    $stmt3 = $conn->query($showUnfinished);
 ?>
 <!DOCTYPE html>
 <html lang="FR-fr">
@@ -30,12 +44,15 @@ include 'logins/connect.php';
             <input type="text" placeholder="Ajoutez une tâche">
         </div>
         <ol>
-            <li><span class="checklist"></span><p>Finir le portfolio</p></li>
-            <li><span class="checklist"></span><p>Faire des biscuits</p></li>
-            <li><span class="checklist"></span><p>Trouver un stage</p></li>
-            <li><span class="checklist"></span><p>Lire la doc php</p></li>
-            <li><span class="checklist"></span><p>Créer un README</p></li>
-            <li><span class="checklist"></span><p>Arriver à l'heure (Loïc)</p></li>
+            <?php while($row = $stmt1->fetch(PDO::FETCH_ASSOC)) : ?>
+            <li><img src="<?php echo htmlspecialchars($row['url_image']); ?>"><p><?php echo htmlspecialchars($row['nom']); ?></p></li>
+            <?php endwhile; ?>
+            <?php while($row = $stmt2->fetch(PDO::FETCH_ASSOC)) : ?>
+            <li><img src="<?php echo htmlspecialchars($row['url_image']); ?>"><p><?php echo htmlspecialchars($row['nom']); ?></p></li>
+            <?php endwhile; ?>
+            <?php while($row = $stmt3->fetch(PDO::FETCH_ASSOC)) : ?>
+            <li><img src="<?php echo htmlspecialchars($row['url_image']); ?>"><p><?php echo htmlspecialchars($row['nom']); ?></p></li>
+            <?php endwhile; ?>
         </ol>
     </section>
     <section>
@@ -43,9 +60,9 @@ include 'logins/connect.php';
             <p>5 tâches restantes</p>
         </div>
         <div>
-            <button>Toutes</button>
-            <button>En cours</button>
-            <button>Finies</button>
+            <button name="all">Toutes</button>
+            <button name="unfinished">En cours</button>
+            <button name="finished">Finies</button>
         </div>
     </section>
 </main>
@@ -55,3 +72,15 @@ include 'logins/connect.php';
 <script src="app.js"></script>
 </body>
 </html>
+
+<?php
+
+if($stmt1 === false){
+  die("Erreur");
+ }
+ 
+}catch (PDOException $e){
+  die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage());
+}
+
+?>
